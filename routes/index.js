c9497b3777ca586f372b4248f16da37c55ad0ca2
Titanium <xingchenlyx@qq.com>
2018-07-27 20:26:03 +0800

    改

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var todoList = require("../database/model/todoList");
 
 router.get('/todo', function(req, res, next) {
-  todoList.find({}).exec((err,data) => {  //这为‘查’（增删改查）
+  todoList.find({}).then((data) => {  //这为‘查’（增删改查）
       // console.log(err,data);
       // res.render('index', { title: 'Express',arr: data});
       // // 查数据库肯定是异步过程
@@ -16,6 +16,7 @@ router.get('/todo', function(req, res, next) {
 
 router.post("/todo", function(req,res){
   let todo = req.body;
+  todo.isDone = false;
   todoList.create(todo).then(data => {   //这为‘增’
     res.json({
         code: 200,
@@ -24,9 +25,26 @@ router.post("/todo", function(req,res){
   })
 });
 
-router.patch("/todo:id",(req,res) => {
+router.patch("/todo/:id",(req,res) => {
   let id = req.params.id;
-  res.json({})
+  let title = req.body.title;
+  let isDone = req.body.isDone==1?true:false;
+    todoList.update({_id: id},{isDone,title}).then(data => {
+        res.json({
+            code: 200,
+            msg: "修改成功"
+        })
+    })
+})
+
+router.delete("/todo/:id",(req,res) => {
+    let id = req.params.id;
+    todoList.remove({_id: id}).then(data => {
+        res.json({
+            code: 200,
+            msg: "删除成功"
+        })
+    })
 })
 
 module.exports = router;
